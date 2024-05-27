46c79e9f7b2ad44c4bb0e69d1ecc2be07c7f6892
Nick Fraser <icanlosh@gmail.com>
2024-05-27 11:58:00 +0100

    Squashed commit of the following:
    commit 6d9375189ada857332eed010361e491aa252a5ac
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Mar 21 16:08:42 2024 +0000
    
        Fix: Added position ids to the inputs, added workaround for wrong KV Cache shape
    
    commit 276566f3bab63dc607493b02506fe1d51f35cb7f
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Mar 21 16:03:14 2024 +0000
    
        Fix: Enable FX in quant config that dataset returns past key/values
    
    commit 5dba59ade937d592b71c0caed4c656e381d9d1bb
    Merge: 393da4a 9339adb
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Mar 14 08:56:53 2024 +0000
    
        Merge remote-tracking branch 'origin/main' into feat/validate_onnx
    
    commit 393da4ad8da9a1b01d6e6157e38c3b07b72d3ead
    Merge: 9ef4f01 b0e41f7
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Fri Mar 8 14:54:17 2024 +0000
    
        Merge branch 'main' into feat/validate_onnx
    
    commit 9ef4f010aa79d713caa72092bb3dd6db0229e887
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Fri Mar 1 17:08:07 2024 +0000
    
        Fix (brevitas/example): Updated args description
    
    commit b0ca3fb027adbcfc843990e3d892455bff7da6e0
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Fri Mar 1 17:07:26 2024 +0000
    
        Fix (brevitas/example): bugfix in onnx return value
    
    commit e8e57c7cdf6829651ffc2d420ff2764f903c407a
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Feb 29 16:12:50 2024 +0000
    
        Fix (brevitas/example): Updated script description
    
    commit fd0a0aa88d8da94e833878b5f8de3c9b5e1f5409
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Feb 29 15:59:01 2024 +0000
    
        Fix (brevitas/examples): fixed style
    
    commit 1d99f612decda72d85a1ad117b6be5d1d520a08c
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Feb 29 15:45:14 2024 +0000
    
        Feat (brevitas/example): Moved ONNX validation main to main function
    
    commit c0b96fa73b3f58ee73aed8b26fb9474eb0c984c6
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Thu Feb 15 18:15:44 2024 +0000
    
        Fix: Updated model location
    
    commit d488969a87c4fd5ac4c9b1919631b60d6ffd8a5c
    Author: Nick Fraser <icanlosh@gmail.com>
    Date:   Wed Feb 7 18:40:03 2024 +0000
    
        Feat: added simple script to validate onnx perplexity

---
+++ examples/quantization/brevitas/validate_onnx.py
@@ -0,0 +1,167 @@
+# Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Licensed under the MIT License.
+
+import random
+from argparse import ArgumentParser
+
+import numpy as np
+import onnxruntime as ort
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+from optimum.amd import BrevitasQuantizationConfig
+from optimum.amd.brevitas.data_utils import get_dataset_for_model
+
+# from transformers import AutoConfig
+from transformers import AutoTokenizer
+
+
+@torch.no_grad()
+def onnx_compute_perplexity(onnx_file, data, context_length: int, tokenizer, seed: int = 0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.random.manual_seed(seed)
+
+    ort_sess = ort.InferenceSession(onnx_file, providers=["CPUExecutionProvider"])
+    # ort_sess = ort.InferenceSession(onnx_file, providers=['CUDAExecutionProvider'])
+
+    cross_entropy_loss = nn.CrossEntropyLoss()
+
+    nlls = []
+    for sample in tqdm(data, desc="Computing perplexity..."):
+        sample_length = sample["input_ids"].shape[1]
+        for start_index in range(0, sample_length, context_length * 2):
+            end_index = min(start_index + sample_length, sample_length - 1)
+
+            subsample = {
+                "input_ids": sample["input_ids"][:, start_index : end_index + 1],
+                "attention_mask": sample["attention_mask"][:, start_index : end_index + 1],
+            }
+
+            # In case we are using torch.fx, we can not have optional inputs, and we have traced the model with past_key_values inputs, thus we need them here as well.
+            if "past_key_values" in sample:
+                subsample["past_key_values"] = sample["past_key_values"]
+
+            # Add BOS token.
+            subsample["input_ids"][:, 0] = tokenizer.bos_token_id
+
+            # Put subsample on CPU
+            onnx_subsample = {}
+            for k in subsample.keys():
+                if isinstance(subsample[k], tuple):
+                    for j in range(len(subsample[k])):
+                        onnx_subsample[f"past_key_values.{j}.key"] = subsample[k][j][0].cpu().numpy()
+                        onnx_subsample[f"past_key_values.{j}.value"] = subsample[k][j][1].cpu().numpy()
+                else:
+                    onnx_subsample[k] = subsample[k].cpu().numpy()
+
+            for inp in ort_sess.get_inputs():
+                name = inp.name
+                if name in onnx_subsample.keys():
+                    # Workaround for incorrect shape
+                    if "past_key_values" in name:
+                        expected_shape = (
+                            onnx_subsample[name].shape[0],
+                            inp.shape[1],
+                            onnx_subsample[name].shape[2],
+                            inp.shape[3],
+                        )
+                        if not expected_shape == onnx_subsample[name].shape:
+                            print(f"Warning: override {name} shape from {onnx_subsample[name].shape} to {expected_shape}")
+                            onnx_subsample[name] = torch.empty(size=expected_shape).cpu().numpy()
+                    continue
+                if name in subsample.keys():
+                    print(f"Warning: unknown key: {name} found.")
+                if name == "position_ids":
+                    print(f"Creating default {name} input")
+                    onnx_subsample[name] = torch.arange(start_index, end_index+1).unsqueeze(0).cpu().numpy()
+                    print(f"{name}: {onnx_subsample[name].shape}")
+
+            # lm_logits = model(**subsample)["logits"]
+            onnx_out = ort_sess.run(["logits"], onnx_subsample)
+            lm_logits = torch.tensor(onnx_out[0])
+
+            reference_labels = subsample["input_ids"][:, context_length:]
+
+            shift_logits = lm_logits[:, context_length - 1 : -1]
+
+            # Fuse batch and sequence length dimensions.
+            reference_labels = reference_labels.view(reference_labels.shape[-1])
+            shift_logits = shift_logits.view(-1, shift_logits.shape[-1])
+
+            loss = cross_entropy_loss(shift_logits, reference_labels)
+
+            nlls.append(loss)
+
+    ppl = torch.exp(torch.stack(nlls).mean())
+
+    return ppl
+
+
+def main(args):
+    return_val = {}
+
+    onnx_file = f"{args.onnx_path}/model.onnx"
+    model_name = args.model
+    # model_config = AutoConfig.from_pretrained(args.model)
+    # attention_head_size = int(model_config.hidden_size / model_config.num_attention_heads)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    qconfig = BrevitasQuantizationConfig(apply_weight_equalization=True)  # Actual contents won't matter, except we should enable FX
+    validation_dataset = get_dataset_for_model(
+        args.model,
+        qconfig=qconfig,
+        dataset_name="wikitext2",
+        tokenizer=tokenizer,
+        nsamples=args.nsamples,
+        seqlen=args.seqlen,
+        split="validation",
+        device="cpu",
+        fuse_sequences=args.fuse_sequences,
+    )
+
+    perplexity = onnx_compute_perplexity(
+        onnx_file, validation_dataset, context_length=args.seqlen // 2, tokenizer=tokenizer
+    )
+    print(f"ONNX Perplexity: {perplexity}")
+    return_val = {"onnx_perplexity": perplexity}
+    return return_val
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Test perplexity of 🤗 Transformers models quantized with AMD Brevitas")
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="facebook/opt-125m",
+        help="Model checkpoint to quantize. Can either be a local path to a model folder, or a model on Hugging Face Hub. Example: facebook/opt-125m",
+    )
+    parser.add_argument(
+        "--seqlen",
+        type=int,
+        default=128,
+        help="Sequence length to use during validation (default: %(default)s).",
+    )
+    parser.add_argument(
+        "--nsamples",
+        type=int,
+        default=128,
+        help="Number of samples to use during validation (default: %(default)s).",
+    )
+    parser.add_argument(
+        "--fuse-sequences",
+        action="store_true",
+        default=False,
+        help="Whether to merge the dataset sequences in case they are shorter than the requested number of samples per sequence. This is useful in case you would like to quantize or evaluate on long sequences (default: %(default)s).",
+    )
+    parser.add_argument(
+        "--onnx-path",
+        type=str,
+        default="llm_quantized_onnx",
+        help="Location of the ONNX model (default: %(default)s)",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
